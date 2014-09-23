8b39feeba2b950bdc323dbd48bd6fe5141d80d53
Ben Ellis <ben@oldtownit.com>
2014-09-23 17:45:54 -0500

    Initial commit of WPF widget

---
+++ Source/Standometer/Standometer.UI.WPF/StatusChangeEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standometer.UI.WPF
+{
+	class StatusChangeEvent
+	{
+		public StatusChangeEvent(DateTime eventDate, StartStop startOrStop, Status status)
+		{
+			this.EventDate = eventDate;
+			this.StartOrStop = startOrStop;
+			this.Status = status;
+		}
+
+		public DateTime EventDate { get; private set; }
+		public StartStop StartOrStop { get; private set; }
+		public Status Status { get; private set; }
+	}
+
+	enum StartStop
+	{
+		Start,
+		Stop
+	}
+
+	enum Status
+	{
+		Unknown,
+		Standing,
+		Sitting
+	}
+}
